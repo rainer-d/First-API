f3231b02d15b6d745bd8585b842087397268a590
rainer-d <rainerdoller@gmail.com>
2020-06-17 10:55:31 +0100

    Updates

---
+++ precache-manifest.124e24ea1af8788a1346f98712bba7a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578c9d02e6a50c8adde41bf9ae8f5be0",
+    "url": "/First-API/index.html"
+  },
+  {
+    "revision": "a21d9d09c16b31f717d8",
+    "url": "/First-API/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "608bf4e41ed7c52deeb8",
+    "url": "/First-API/static/css/main.9d04ee10.chunk.css"
+  },
+  {
+    "revision": "a21d9d09c16b31f717d8",
+    "url": "/First-API/static/js/2.27c110c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-API/static/js/2.27c110c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608bf4e41ed7c52deeb8",
+    "url": "/First-API/static/js/main.90e304e3.chunk.js"
+  },
+  {
+    "revision": "e9288e3603370da83f85",
+    "url": "/First-API/static/js/runtime-main.63d0f857.js"
+  },
+  {
+    "revision": "2d398bfb4162dbff91fbfc3e2dd5405b",
+    "url": "/First-API/static/media/SoloistLaserItalic.2d398bfb.otf"
+  }
+]);
